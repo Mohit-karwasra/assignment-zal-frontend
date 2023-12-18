45e9ca3df1237b05e0b10c60c63c2a5145798bb9
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-12-18 22:03:18 +0530

    solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # assignment-zal-frontend
+
+When user enters the input as number and clicks on generate, solid black blocks being equal to number as input are created in a diagonal from top-right to bottom-left
---
+++ main.js
@@ -0,0 +1,25 @@
+function createGrid() {
+	const gridSize = document.getElementById("gridSize").value;
+	const gridContainer = document.getElementById("gridContainer");
+	// start by setting grid container as empty
+	gridContainer.innerHTML = "";
+
+	gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+	gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+	for (let i = 0; i < gridSize; i++) {
+		for (let j = 0; j < gridSize; j++) {
+			const gridItem = document.createElement("div");
+			gridItem.classList.add("grid-item");
+
+			// Sets the background color black based on diagonal's position
+			if (i + j === gridSize - 1) {
+				gridItem.style.backgroundColor = "black";
+			} else {
+				gridItem.style.backgroundColor = "white";
+			}
+
+			gridContainer.appendChild(gridItem);
+		}
+	}
+}
